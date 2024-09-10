a89c6091a469deaedf75c892cedd4d0673c93c65
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-09-10 19:41:54 +0530

    Update 977. Squares of a Sorted Array_Leetcode.java

--- 977. Squares of a Sorted Array_Leetcode.java
+++ 977. Squares of a Sorted Array_Leetcode.java
@@ -1,8 +1,5 @@
 /*
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
-
- 
-
 Example 1:
 
 Input: nums = [-4,-1,0,3,10]
@@ -19,7 +16,7 @@ Constraints:
 
 1 <= nums.length <= 104
 -104 <= nums[i] <= 104
-nums is sorted in non-decreasing order.
+
 */
 
 class Solution {
