133379276b22431a71142d17762d2971f13015dd
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-09-25 14:03:22 +0530

    Create 1859. Sorting the Sentence_Leetcode.java

---
+++ 1859. Sorting the Sentence_Leetcode.java
@@ -0,0 +1,53 @@
+/*
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+
+ 
+
+Example 1:
+
+Input: s = "is2 sentence4 This1 a3"
+Output: "This is a sentence"
+Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+Example 2:
+
+Input: s = "Myself2 Me1 I4 and3"
+Output: "Me Myself and I"
+Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+*/
+
+class Solution {
+    public String sortSentence(String s) {
+        StringBuilder sb=new StringBuilder();
+        String arr[]=s.split(" ");
+        int i=0,j=0,a=49;
+        while(i<arr.length){
+            String str=arr[i];
+            if(a == str.charAt(str.length()-1)){
+                sb.append(str);
+                j++;
+                a++;
+            }
+            i++;
+            if(i==arr.length){
+                i=0;
+            }
+            if(j==arr.length){
+                break;
+            }
+        }
+        for(i=0;i<sb.length();i++){
+            if(sb.charAt(i)>=49 && sb.charAt(i)<=57){
+                sb.replace(i,i+1," ");
+            }
+        }
+        
+        sb.deleteCharAt(sb.length()-1);
+        
+        return sb.toString();
+    }
+}
