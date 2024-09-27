1dc10726c87bea545b4ecb5a899990d7114c3104
MATHESH M <156519287+Alphamale916@users.noreply.github.com>
2024-09-27 20:40:07 +0530

    Update Pattern2.java

--- Pattern2.java
+++ Pattern2.java
@@ -1,6 +1,4 @@
-/*
-Question 2
-Input: 5
+/* Question 2 -- Input: 5
 Output:
     1
    21
@@ -9,7 +7,6 @@ Output:
 54321
 */
 import java.util.Scanner;
-
 public class Pattern2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
